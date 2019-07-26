0fbbe81cb9e3217785062d124392ad325456f1c1
Student <student@Makerss-MacBook-Pro.local>
2019-07-26 17:00:34 +0100

    Refactoring

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,16 +3,23 @@
 class DockingStation 
   # Initialize array
   def initialize
-    @bike = []
+    @bikes = []
   end
-    # Method to release bike when bikes are available
-    def release_bike
-      fail 'No bikes available' if @bike.empty?
-      @bike.pop
-    end
-    # Method to dock bike if dock doesn't already contain a bike
-    def dock(bike)
-      fail 'Docking station full' if @bike.count >= 20
-      @bike << bike
-    end
+  # Method to release bike when bikes are available
+  def release_bike
+    fail 'No bikes available' if empty?
+    @bikes.pop
+  end
+  # Method to dock bike if dock doesn't already contain a bike
+  def dock(bike)
+    fail 'Docking station full' if full?
+    @bikes << bike
+  end
+  # Methods to describe whether 
+  def full?
+    @bikes.count >= 20
+  end
+  def empty?
+    @bikes.empty?
+  end
 end
